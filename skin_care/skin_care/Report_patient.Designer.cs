9d3e9e9ce47ea413227206c44718341da83feeb8
VC <tommy8595>
2018-08-22 22:25:23 +0700

    report patient

---
+++ skin_care/skin_care/Report_patient.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace skin_care
+{
+    partial class Report_patient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Report_patient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 630);
+            this.Name = "Report_patient";
+            this.Text = "Report_patient";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
