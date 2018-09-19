ff984597b6cb438c9279ec1c40a7abcd20cc3a2d
VC <tommy8595>
2018-09-19 15:42:46 +0700

    finish all

--- Skincare Management System/Skincare Management System/frm_History.cs
+++ Skincare Management System/Skincare Management System/frm_History.cs
@@ -124,7 +124,7 @@ namespace Skincare_Management_System
         private void btn_print_history_Click(object sender, EventArgs e)
         {
             frm_ReportPatient rp = new frm_ReportPatient
-            (int.Parse(dgv_history.CurrentRow.Cells[0].Value.ToString()), int.Parse(txt_id_history.Text),txt_name_history.Text);
+            (int.Parse(dgv_history.CurrentRow.Cells[7].Value.ToString()), int.Parse(txt_id_history.Text),txt_name_history.Text);
             rp.Show();
         }
        
